225758cd2392f7c827957a109740cace2ec16050
Francesco <francesco.arfilli@gmail.com>
2024-07-18 10:48:53 +0200

    V0.3

--- Form_VarHandler.Designer.vb
+++ Form_VarHandler.Designer.vb
@@ -91,7 +91,7 @@ Partial Class Form_VarHandler
         Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form_VarHandler"
-        Me.Text = "Solid Edge VarHandler v0.2"
+        Me.Text = "Solid Edge VarHandler v0.3"
         Me.TopMost = True
         Me.ToolStrip1.ResumeLayout(False)
         Me.ToolStrip1.PerformLayout()
