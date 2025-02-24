d1cf00690fae3f744d2721060aee14a9273350b9
rmcanany <64286960+rmcanany@users.noreply.github.com>
2025-02-24 16:01:32 -0600

    Add variable steps to workflow

--- Form_WorkFlow.vb
+++ Form_WorkFlow.vb
@@ -118,7 +118,8 @@ Public Class Form_WorkFlow
 
                 SetSteps(StepEvent)
 
-                For i = 1 To 20
+                'For i = 1 To 20
+                For i = 1 To StepEvent.steps
 
                     Form_VarHandler.objDoc.Parent.DelayCompute = True
 
@@ -147,10 +148,13 @@ Public Class Form_WorkFlow
 
     Private Sub SetSteps(stepEvent As UC_WorkFlowEvent)
 
+        Dim tmpSteps = stepEvent.steps
+
         For Each tmpRow As DataGridViewRow In stepEvent.DG_Variables.Rows
 
             Dim tmpVariable As Object = tmpRow.Cells("objVar").Value
-            Dim stepValue As Double = (CDbl(tmpRow.Cells("Value").Value) - UC_Slider.CadToValue(tmpVariable.Value, tmpVariable.UnitsType, LengthUnits)) / 20
+            'Dim stepValue As Double = (CDbl(tmpRow.Cells("Value").Value) - UC_Slider.CadToValue(tmpVariable.Value, tmpVariable.UnitsType, LengthUnits)) / 20
+            Dim stepValue As Double = (CDbl(tmpRow.Cells("Value").Value) - UC_Slider.CadToValue(tmpVariable.Value, tmpVariable.UnitsType, LengthUnits)) / tmpSteps
 
             tmpRow.Tag = stepValue
 
